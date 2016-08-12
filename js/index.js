38e4a1da2f28cc2ae6c46f2ebf0e16ca062674e6
aadams22 <ashleigh.adams22@gmail.com>
2016-08-12 12:56:22 -0500

    init commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log('hello world');
+$(function(){
+	var skills = ["HTML5", "CSS3", "C", "Arduino", "Javascript", "jQuery", "AngularJS", "Ruby", "Rails", "MySql", "PostGreSQL", "MongoDB", "php", "WordPress"];	
+
+	
+
+});
+
