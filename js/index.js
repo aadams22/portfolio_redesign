42d39bcac3307ef3fa173a214d4ea54af0044083
aadams22 <ashleigh.adams22@gmail.com>
2016-08-12 13:39:57 -0500

    adding mouseover to sections

--- js/index.js
+++ js/index.js
@@ -2,6 +2,14 @@ console.log('hello world');
 $(function(){
 	var skills = ["HTML5", "CSS3", "C", "Arduino", "Javascript", "jQuery", "AngularJS", "Ruby", "Rails", "MySql", "PostGreSQL", "MongoDB", "php", "WordPress"];	
 
+	$("section")
+	.mouseover(function(e){
+		console.log("it clicked");
+		$(this).animate( {height: "500px"}, 550);
+	})
+	.mouseleave(function(e){
+		$(this).animate( {height: "100px"}, 550);
+	});
 	
 
 });
