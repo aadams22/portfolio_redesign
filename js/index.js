069bd5df9e5417d5d54695ad9676ebc65db2bb52
aadams22 <ashleigh.adams22@gmail.com>
2016-08-16 18:52:17 -0500

    adding an animation that causes smooth scroll to section on which the user has hovered

--- js/index.js
+++ js/index.js
@@ -1,34 +1,31 @@
 // console.log('hello world');
 $(function(){
-	// var skills = ["HTML5", "CSS3", "C", "Arduino", "Javascript", "jQuery", "AngularJS", "Ruby", "Rails", "MySql", "PostGreSQL", "MongoDB", "php", "WordPress"];	
 
 	$("section")
 	.mouseover(function(e){
-		console.log("it clicked");
+		//creates hash
+		var h = "#" + $(this).attr("id");
+		//causes the section to grow
 		$(this).animate( { height: "800px" }, 0);
+		//scrolls down to the div on which the user has hovered
+		$("html, body").animate({
+			scrollTop: $(h).offset().top
+		}, 1000);
+
 	})
 	.mouseleave(function(e){
+		//causes the section to become smaller
 		$(this).animate( { height: "100px" }, 0);
 	});
-	
-	// !! USE FOR DEVELOPMENT !!x
-	// $("section")
-	// .click(function(e){
-	// 	console.log("header");
-	// 	$(this).animate( { height: "800px" }, 0);
-	// });
-
 
 	//for mobile device to close all tabs
 	$("header")
 	.click(function(e){
-		console.log("header");
 		$("section").animate( { height: "100px" }, 0);
 	});
 
-
+	//adds and removes disabled class from projects to reveal overlay
 	$("#projects-container > div").hover(function(){
-		// console.log("this ", $(this));
 		$(this).children().toggleClass("disabled");
 	});
 
