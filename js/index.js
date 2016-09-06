732f0e66953b63504b70378fbf296f5c8f8deae3
aadams22 <ashleigh.adams22@gmail.com>
2016-09-06 12:05:47 -0500

    because I can

--- js/index.js
+++ js/index.js
@@ -1,28 +1,38 @@
-// console.log('hello world');
+
 $(function(){
 
 	$("section")
 	.mouseover(function(e){
+		var hasSlid = 0;
 		//creates hash
-		var h = "#" + $(this).attr("id");
-		
-		//scrolls down to the div on which the user has hovered
-		// $("html, body").animate({
-		// 	scrollTop: $(h).offset().top
-		// }, 100);
+		var id = "#" + $(this).attr("id");
+		console.log(id);
+		//causes the section to grow 
+		//!!Should probably move this to a class toggle situation...will do later.
+		var h = 0;
+		if(id == "#projects") h = "1020px";
+		else if(id == "#contact") h = "400px";
+		else h = "800px";
 
-		//causes the section to grow
-		if(h == "#projects") { 
-			$(this).animate( { height: "1020px" }, 0);
-		}else { 
-			$(this).animate( { height: "800px" }, 0);
-		}
+		console.log("after slide up: ", hasSlid);
+		$(this).animate( { height: h }, 0);
 
+		console.log("before slide: ", hasSlid);
+		if (!hasSlid) {
+			// scrolls down to the div on which the user has hovered
+			$("html, body").animate({
+				scrollTop: $(id).offset().top
+			}, 100);
+		};
+		hasSlid = 1;
+ 
 	})
 	.mouseleave(function(e){
 		//causes the section to become smaller
 		$(this).animate( { height: "100px" }, 0);
-		h = "";
+		id = "";
+		hasSlid = 0;
+		console.log("on mouseleave: ", hasSlid);
 	});
 
 	//for mobile device to close all tabs
